cf8f4911f9706c6be2f55e7f5ec7e8e040dc094d
w3aseL <noahtemp13@gmail.com>
2020-12-01 08:15:16 -0600

    Added full database with insert queries

--- sql/invoice.sql
+++ sql/invoice.sql
@@ -2,6 +2,6 @@ CREATE TABLE Invoice (
     id INTEGER NOT NULL PRIMARY KEY,
     user_id INTEGER,
     total DECIMAL(12, 2) NOT NULL,
-    purchase_date DATETIME NOT NULL
+    purchase_date DATETIME NOT NULL,
     FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE SET NULL
 );
